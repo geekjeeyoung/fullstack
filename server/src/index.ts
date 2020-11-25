a5b4ec1b0db8119899a465f2bb5863c96916d967
Candice Han <jeeyoung.han82@gmail.com>
2020-11-26 00:07:23 +0900

    SET session & cookie

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,18 +10,45 @@ import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 yrs
+        httpOnly: true,
+        secure: __prod__,
+        sameSite: "lax",
+      },
+      secret: "komdori and penggoo",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }), // accessible from all the resolvers
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }), // accessible from all the resolvers
   });
 
   apolloServer.applyMiddleware({ app });
