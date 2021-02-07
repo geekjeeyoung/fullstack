46a251bdd84b880661c92bfa754a8d97053a57cb
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-02-07 15:50:07 +0900

    Start from the scratch

--- README.md
+++
@@ -1,8 +0,0 @@
-# lireddit
-
-# ONLY USE Yarn
-
-# graphql playground 에서 cookie 값이 보이게 하려면 설정에서 "request.credentials": "include" 설정하기
-
-Start from
-**4:35:31 Change Password**
--- server/dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.COOKIE_NAME = exports.PORT = exports.__prod__ = void 0;
-exports.__prod__ = process.env.NODE_ENV === "production";
-exports.PORT = 4000;
-exports.COOKIE_NAME = "qid";
-//# sourceMappingURL=constants.js.map
