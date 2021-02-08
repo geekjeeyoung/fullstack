a97594eef645655b1d2bed6e756508429621ebb2
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-02-08 10:17:20 +0900

    ADD migration

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,13 +1,28 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const typeorm_1 = require("typeorm");
+require("dotenv-safe/config");
+const Post_1 = require("./entities/Post");
+const User_1 = require("./entities/User");
+const Upvote_1 = require("./entities/Upvote");
+const main = async () => {
+    const conn = await typeorm_1.createConnection({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        logging: true,
+        migrations: [path_1.default.join(__dirname, "./migrations/*")],
+        entities: [Post_1.Post, User_1.User, Upvote_1.Upvote],
+    });
+    const app = express_1.default();
+    app.listen(parseInt(process.env.PORT), () => {
+        console.log("server started on localhost:%d", process.env.PORT);
     });
 };
-const main = () => __awaiter(this, void 0, void 0, function* () { });
 main().catch((err) => {
     console.log(err);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,28 @@
-const main = async () => {};
+import express from "express";
+import path from "path";
+import { createConnection } from "typeorm";
+import "dotenv-safe/config";
+import { Post } from "./entities/Post";
+import { User } from "./entities/User";
+import { Upvote } from "./entities/Upvote";
+
+const main = async () => {
+  const conn = await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: true,
+    // synchronize: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [Post, User, Upvote],
+  });
+
+  //   await conn.runMigrations();
+
+  const app = express();
+  app.listen(parseInt(process.env.PORT), () => {
+    console.log("server started on localhost:%d", process.env.PORT);
+  });
+};
 
 main().catch((err) => {
   console.log(err);
