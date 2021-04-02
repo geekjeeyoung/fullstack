884fd841661afbaacc8cfdcb239c04ba65d267ef
Candice Han <jeeyoung.han82@gmail.com>
2021-04-03 00:13:43 +0900

    Server setup

---
+++ server/src/index.ts
@@ -0,0 +1,80 @@
+import "reflect-metadata";
+import "dotenv-safe/config";
+import { createConnection } from "typeorm";
+import path from "path";
+import express from "express";
+import connectRedis from "connect-redis";
+import session from "express-session";
+import Redis from "ioredis";
+import cors from "cors";
+import { COOKIE_NAME, __prod__ } from "./constants";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+const main = async () => {
+  const connection = await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [], //TODO
+  });
+
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redis = new Redis(process.env.REDIS_URL);
+
+  app.set("trust proxy", 1);
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+    })
+  );
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redis,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__,
+        domain: __prod__ ? ".codeponder.com" : undefined,
+      },
+      saveUninitialized: false,
+      secret: process.env.SESSION_SECRET!!,
+      resave: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+      redis,
+      //TODO
+    }),
+  });
+
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
+
+  app.listen(parseInt(process.env.PORT!!), () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
